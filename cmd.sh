f1f5bdb897480025d5dc9881a0bed7a558d4b5e2
Mohammad <mohammad4cj@gmail.com>
2021-01-29 06:25:11 +0000

    c

--- cmd.sh
+++ cmd.sh
@@ -2,14 +2,12 @@
 
 name_secret="serviceaccount-bigquery-storag"
 
-mkdir -p ./creds 
+#mkdir -p ./creds 
 
 #  Add secret "SERVICEACCOUNT_bigquery" to /security/secret-manager
 #  Enable "Secret Manager" in cloud-build/settings
 
-gcloud secrets versions access latest --secret=$name_secret --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./creds/serviceaccount.json
+gcloud secrets versions access latest --secret=$name_secret --format='get(payload.data)' | tr '_-' '/+' | base64 -d > ./environments/serviceaccount.json
 
-gcloud auth activate-service-account --key-file ./creds/serviceaccount.json
-
-cp ./creds/serviceaccount.json ./environments/serviceaccount.json
+gcloud auth activate-service-account --key-file ./environments/serviceaccount.json
 
